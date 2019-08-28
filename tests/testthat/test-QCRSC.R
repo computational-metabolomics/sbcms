c2dddcd487fd4651baa034fdfd2eff7fde0d7e07
andzajan <andzajan@gmail.com>
2019-08-28 17:37:05 +0100

    New tests added

--- tests/testthat/test-QCRSC.R
+++ tests/testthat/test-QCRSC.R
@@ -13,3 +13,15 @@ test_that ("QC-RSC returns expected output", {
   expect_equal (out, testData$QCRSC)
 
 })
+
+test_that ("QC-RSC returns error if QC sample label is wrong", {
+  
+  classes <- sbcdata$class
+  classes[classes == "QC"] <- "Quality"
+  batch <- sbcdata$batch
+  order <- c(1:nrow(sbcdata$data))
+  data <- t(sbcdata$data[, 1:20])
+  
+  expect_error(QCRSC(df = data, order = order, batch = batch, classes = classes,
+               spar = 0, minQC = 4))
+})
--- tests/testthat/test-sbcWrapper.R
+++ tests/testthat/test-sbcWrapper.R
@@ -17,3 +17,57 @@ test_that ("sbcWrapper returns expected output", {
   expect_equal (out, testData$sbcWrapper)
 
 })
+
+test_that ("sbcWrapper returns expected output, with predefined spar value", {
+  
+  classes <- sbcdata$class
+  batch <- sbcdata$batch
+  order <- c(1:nrow(sbcdata$data))
+  
+  Data <- t(sbcdata$data)
+  qcData <- Data[,classes=="QC"]
+  qc_batch <- batch[classes=="QC"]
+  qc_order <- order[classes=="QC"]
+  
+  out <- sbcWrapper(id=4, qcData=qcData, order=order, qcOrder=qc_order, qcBatch=qc_batch,
+                    log=TRUE, spar=1, batch=batch, minQC=4)
+  
+  expect_equal (out, testData$sbcWrapper_spar_1)
+  
+})
+
+test_that ("sbcWrapper returns NA if not enough data point are measured", {
+  
+  classes <- sbcdata$class
+  batch <- sbcdata$batch
+  order <- c(1:nrow(sbcdata$data))
+  
+  Data <- t(sbcdata$data)
+  qcData <- Data[,classes=="QC"]
+  qc_batch <- batch[classes=="QC"]
+  qc_order <- order[classes=="QC"]
+  
+  qcData[4, -c(sample(38, 3))] <- NA
+  
+  out <- sbcWrapper(id=4, qcData=qcData, order=order, qcOrder=qc_order, qcBatch=qc_batch,
+                    log=TRUE, spar=1, batch=batch, minQC=4)
+  
+  expect_true (all(is.na(out)))
+  
+})
+
+test_that ("sbcWrapper returns error", {
+  classes <- sbcdata$class
+  batch <- sbcdata$batch
+  order <- c(1:nrow(sbcdata$data))
+  
+  Data <- t(sbcdata$data)
+  qcData <- Data[,classes=="QC"]
+  qc_batch <- batch[classes=="QC"]
+  qc_order <- order[classes=="QC"]
+  
+  dim(qcData) <- NULL
+  
+  expect_error(sbcWrapper(id=4, qcData=qcData, order=order, qcOrder=qc_order, qcBatch=qc_batch,
+                    log=TRUE, spar=1, batch=batch, minQC=4))
+})
